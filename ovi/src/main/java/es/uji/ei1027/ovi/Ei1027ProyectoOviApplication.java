2f30ad17e0a3a8c333bd54540431ae87ad62b30b
Mario de  la Horra <al416129@uji.es>
2026-03-14 17:36:08 +0100

    Creacion del Proyecto
    ---Persona : Mario
    ---Que incluye ? :
    El proyecto ya inicializado y listo para empezar a trabajar sobre el

---
+++ ovi/src/main/java/es/uji/ei1027/ovi/Ei1027ProyectoOviApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.ovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ei1027ProyectoOviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ei1027ProyectoOviApplication.class, args);
+	}
+
+}
---
+++ ovi/src/main/java/es/uji/ei1027/ovi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.ovi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ei1027ProyectoOviApplication.class);
+	}
+
+}
